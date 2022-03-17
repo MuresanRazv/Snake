40286b72518627624cf6e4a34852591d57917e3c
Muresan Razvan <67145369+MuresanRazv@users.noreply.github.com>
2022-03-18 01:11:14 +0200

    Add files via upload

---
+++ snake.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'snake.exe' with Python DLL being found.
+set PATH=c:\users\SENNNZ~1\ANACON~1;%PATH%
+set PYTHONHOME=c:\users\SENNNZ~1\ANACON~1
+"%~dp0.\snake.exe" %*
